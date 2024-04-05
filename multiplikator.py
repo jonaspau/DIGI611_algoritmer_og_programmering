277277783d22c1dccc470e29ea33847f734db84d
jonaspauls1 <121925348+jonaspauls1@users.noreply.github.com>
2024-04-05 09:53:40 +0200

    Lastet opp første filer til modul 1

---
+++ multiplikator.py
@@ -0,0 +1,8 @@
+def multiplikator():
+    tall_1 = float(input("Første tall: "))
+    tall_2 = float(input("Andre tall: "))
+    sum = tall_1 * tall_2
+
+    print(f"{tall_1} * {tall_2} = {sum}")
+
+multiplikator()
---
+++ sirkel.py
@@ -0,0 +1,11 @@
+def sirkel(radius):
+    pi = 3.14
+
+    omkrets = 2 * pi * radius
+    areal = pi * radius * radius
+
+    return(areal, omkrets)
+
+sirkel2 = sirkel(2)
+sirkel3 = sirkel(3)
+sirkel4 = sirkel(4)
